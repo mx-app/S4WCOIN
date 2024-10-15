1518bc60bb045e0219f0fab6d99d3987c8cf3e34
mx-app <elsoghiarhouda@gmail.com>
2024-10-15 06:27:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -828,29 +828,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
 document.addEventListener('DOMContentLoaded', function() {
   const playGameButton = document.getElementById('playGameButton');
-  const gamePage = document.getElementById('gamePage');
-  const gameContainer = document.getElementById('gameContainer');
 
   playGameButton.addEventListener('click', function() {
-    fetch('Dog.html')
-      .then(response => response.text())
-      .then(data => {
-        // إخفاء باقي عناصر الصفحة
-        gamePage.style.display = 'none';
-
-        // عرض اللعبة بشكل كامل
-        gameContainer.innerHTML = data;
-        gameContainer.style.display = 'block';
-        gameContainer.style.position = 'fixed';
-        gameContainer.style.top = '0';
-        gameContainer.style.left = '0';
-        gameContainer.style.width = '100vw';
-        gameContainer.style.height = '100vh';
-        gameContainer.style.zIndex = '9999'; // التأكد من أن اللعبة فوق جميع العناصر
-      });
+    window.open('Dog.html', '_blank'); // فتح اللعبة في تبويب جديد
   });
 });
 
