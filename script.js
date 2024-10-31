372c36e9c301abc6433b0d3977ea3537b36adcaa
mx-app <elsoghiarhouda@gmail.com>
2024-10-31 10:29:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1762,14 +1762,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+// تهيئة TonConnectUI وربط الزر
+const tonConnectUI = new TonConnectUI();
+
+// الوصول إلى عنصر الزر وضبط أنماطه
 const tonConnectButton = document.getElementById("ton-connect");
 tonConnectButton.style.width = "88%";
 tonConnectButton.style.height = "50px";
-tonConnectButton.style.margin = "auto";
 tonConnectButton.style.display = "flex";
 tonConnectButton.style.alignItems = "center";
 tonConnectButton.style.justifyContent = "center";
-
+tonConnectButton.style.margin = "0 auto";
 
 
 
