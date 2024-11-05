40b34cff73f6bb5d654b459593ae096684c0d5ab
mx-app <elsoghiarhouda@gmail.com>
2024-11-05 20:06:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1040,8 +1040,8 @@ function initializeTelegramIntegration() {
 ///////////////////////////////////////
 
 
-//localStorage.removeItem('gameState'); // مسح حالة اللعبة
-//loadGameState(); // إعادة تحميل حالة اللعبة
+localStorage.removeItem('gameState'); // مسح حالة اللعبة
+loadGameState(); // إعادة تحميل حالة اللعبة
 
 
 //////////////////////////////////////////////////////////
