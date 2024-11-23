f84878ac41c98eb1cddc27e8671a0092fbe9ee06
SAW-COIN <abasfacre@gmail.com>
2024-11-23 09:35:50 +0200

    Update energy.js

--- energy.js
+++ energy.js
@@ -245,3 +245,18 @@ document.addEventListener('DOMContentLoaded', async () => {
         uiElements.updateAttemptsElement.innerText = `${gameState.autClickCount}/2`;
     }
 });
+
+
+const AdController = window.Adsgram.init({ blockId: "int-5511" });
+            const button = document.getElementById('ad');
+            button.addEventListener('click', () => {
+                AdController.show().then((result) => {
+                    // user watch ad till the end or close it in interstitial format
+                    // your code to reward user for rewarded format
+                    alert('Reward');
+                }).catch((result) => {
+                    // user get error during playing ad
+                    // do nothing or whatever you want
+                    alert(JSON.stringify(result, null, 4));
+                })
+            })
