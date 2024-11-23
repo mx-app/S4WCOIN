e53185a83d435f4369bb1a687ccd036bd747c5c2
SAW-COIN <abasfacre@gmail.com>
2024-11-23 09:33:52 +0200

    Update script-sdgbjbnnjbjbjbj.js

--- script-sdgbjbnnjbjbjbj.js
+++ script-sdgbjbnnjbjbjbj.js
@@ -2536,22 +2536,6 @@ document.addEventListener('DOMContentLoaded', handleInvite);
 
 
 
-const AdController = window.Adsgram.init({ blockId: "int-5511" });
-            const button = document.getElementById('ad');
-            button.addEventListener('click', () => {
-                AdController.show().then((result) => {
-                    // user watch ad till the end or close it in interstitial format
-                    // your code to reward user for rewarded format
-                    alert('Reward');
-                }).catch((result) => {
-                    // user get error during playing ad
-                    // do nothing or whatever you want
-                    alert(JSON.stringify(result, null, 4));
-                })
-            })
-
-
-
 
 
 
